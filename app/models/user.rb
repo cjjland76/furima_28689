0def49a5a7ef635498af89ab89a6c410836d77ec
cjjland76 <cjjland76@yahoo.co.jp>
2020-08-18 21:46:26 +0900

    単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    password_REGEX = /\A[a-z0-9]+\z/i.freeze
+    name_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+    name_kana_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/.freeze
+
     validates :nickname
     validates :email
-    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "is invald. " }, length: { minimum: 6 }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :password, format: { with: password_REGEX, message: 'is invald. ' }, length: { minimum: 6 }
+    validates :last_name, format: { with: name_REGEX, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format: { with: name_REGEX, message: 'is invalid. Input full-width characters.' }
+    validates :last_name_kana, format: { with: name_kana_REGEX, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name_kana, format: { with: name_kana_REGEX, message: 'is invalid. Input full-width katakana characters.' }
     validates :birthday
   end
-  
+
   # with_options presence: true do
   #   validates :nickname
   #   validates :email
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {"たろう"}
-    first_name            {"やまだ"}
-    last_name_kana        {"カナ"}
-    first_name_kana       {"カナ"}
-    birthday              {"1111/11/11"}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { 'たろう' }
+    first_name            { 'やまだ' }
+    last_name_kana        { 'カナ' }
+    first_name_kana       { 'カナ' }
+    birthday              { '1111/11/11' }
   end
 end
