24fdece822f718d5df6e8dbff42b3d3e91927d06
cjjland76 <cjjland76@yahoo.co.jp>
2020-08-18 19:33:31 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email
-    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "is invald" }, length: { minimum: 6 }
+    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "is invald. " }, length: { minimum: 6 }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :last_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "is invalid. Input full-width katakana characters."}
