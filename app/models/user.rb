19a57343cbbe3ddec65f15349665ebad7741c403
cjjland76 <cjjland76@yahoo.co.jp>
2020-08-18 22:38:09 +0900

    単体テスト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,15 +18,4 @@ class User < ApplicationRecord
     validates :first_name_kana, format: { with: name_kana_REGEX, message: 'is invalid. Input full-width katakana characters.' }
     validates :birthday
   end
-
-  # with_options presence: true do
-  #   validates :nickname
-  #   validates :email
-  #   validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "is invald" }, length: { minimum: 6 }
-  #   validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  #   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  #   validates :last_name_kana, format: { with: /^([ァ-ン]|ー)+$/, message: "is invalid. Input full-width katakana characters."}
-  #   validates :first_name_kana, format: { with: /^([ァ-ン]|ー)+$/, message: "is invalid. Input full-width katakana characters."}
-  #   validates :birthday
-  # end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,5 +41,17 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+    it 'birthdayが空では登録できない' do
+      @user.birthday = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
+    end
   end
 end
