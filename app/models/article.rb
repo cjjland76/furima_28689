b33f0d0fae4c9ad8d10edb431afe1913d797a312
cjjland76 <cjjland76@yahoo.co.jp>
2020-08-22 16:11:34 +0900

    belong_to_active_hashの修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,10 @@
 class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :status, :delivery_fee, :delivery_area, :delivery_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :delivery_area
+  belongs_to_active_hash :delivery_day
 
   validates :category, :status, :delivery_fee, :delivery_area, :delivery_day, presence: true
 
