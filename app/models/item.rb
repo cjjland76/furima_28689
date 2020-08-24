b721059a29fdf423654f8f0fb5ad8e5f08b0b4b5
cjjland76 <cjjland76@yahoo.co.jp>
2020-08-24 17:00:50 +0900

    商品詳細

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
   has_one :bougth_item
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :delivery_area
+  belongs_to_active_hash :delivery_day
 
   with_options presence: true do
     price_REGEX = /\A[0-9]+\z/.freeze
 
     validates :item_name
     validates :product
-    validates :category
-    validates :status
-    validates :delivery_fee
-    validates :delivery_area
-    validates :delivery_day
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :delivery_area_id
+    validates :delivery_day_id
     validates :price, format: { with: price_REGEX }, numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
   end
 
