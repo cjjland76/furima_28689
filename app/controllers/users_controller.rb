7fefc97b865aea2ac837ea2f00a564a3a099adf1
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-05 09:25:23 +0900

    マイページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+ 
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
     @items = user.items
+    @item = Item.find(params[:id])
   end
 end
