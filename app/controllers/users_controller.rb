210485f723673954890045ceb4609d4cbc1875fc
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-09 16:26:24 +0900

    ユーザー出品一覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
- 
+
+  def index
+    @items = Item.all
+  end
   def show
+    # binding.pry
     user = User.find(params[:id])
     @nickname = user.nickname
-    @items = user.items
-    @item = Item.find(params[:id])
+    @items = user.items.order('created_at DESC')
+    # binding.pry
+    # @item = Item.find(params[:id])
+    # @item = current_user.item.find(params[:id])
   end
 end
