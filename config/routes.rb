6ab80eac77475622954265db5948dc2b4deaa07d
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-15 09:44:53 +0900

    commentコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
  
   resources :items do
     resources :orders
+    resources :comments, only: [:create, :destroy]
     collection do
       get 'search'
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_024020) do
+ActiveRecord::Schema.define(version: 2020_09_15_003014) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2020_08_26_024020) do
     t.integer "delivery_day_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "user_id", null: false
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "delivery_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -106,6 +116,8 @@ ActiveRecord::Schema.define(version: 2020_08_26_024020) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
   add_foreign_key "delivery_addresses", "items"
   add_foreign_key "delivery_addresses", "orders"
   add_foreign_key "items", "users"
